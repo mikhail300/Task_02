6fad7187d17f938941b4a770e18fcfc3cde52de7
mikhail300 <65508527+mikhail300@users.noreply.github.com>
2021-03-28 20:19:46 +0300

    Add files via upload

--- jd1-les04-ex04/src/by/htp/les04/branching/Task04.java
+++ jd1-les04-ex04/src/by/htp/les04/branching/Task04.java
@@ -4,5 +4,22 @@ public class Task04 {
 // 4. Составить программу: равны ли два числа а и b?
 	public static void task() {
 		System.out.println("4. Составить программу: равны ли два числа а и b?");
+		System.out.println();
+		
+		int a;
+		int b;
+		
+		a = 3;
+		b = 4;
+		
+		if (a == b) {
+			
+			System.out.println("\t" + "Числа равны");
+			
+		}else{
+			
+			System.out.println("\t" + "Числа не равны");
+			
+		}
 	}
 }
--- jd1-les04-ex04/src/by/htp/les04/main/Main.java
+++ jd1-les04-ex04/src/by/htp/les04/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		Task01.task();
 		Task02.task();
 		Task03.task();
-//		Task04.task();
+		Task04.task();
 //		Task05.task();
 //		Task06.task();
 //		Task07.task();
