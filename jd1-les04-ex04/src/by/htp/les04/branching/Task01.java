428cfe04a060f241688eba358ae6f0307c1e088a
mikhail300 <65508527+mikhail300@users.noreply.github.com>
2021-04-01 23:55:41 +0300

    Add files via upload

--- jd1-les04-ex04/src/by/htp/les04/branching/Task01.java
+++ jd1-les04-ex04/src/by/htp/les04/branching/Task01.java
@@ -4,28 +4,28 @@ public class Task01 {
 // 1. Составить программу сравнения двух чисел 1 и 2. Если 1 меньше 2 – вывести на экран цифру 7, в противном случае – цифру 8.
 	public static void task() {
 		System.out.println("1. Составить программу сравнения двух чисел 1 и 2. Если 1 меньше 2 – вывести на экран цифру 7, в противном случае – цифру 8.");
-		
+
 		int a;
 		int b;
-		
+
 		a = 6;
 		b = 4;
-		
+
 		System.out.println("\t" + "a = " + a + ", b = " + b);
 		System.out.println();
-		
+
 		if (a < b) {
-			
+
 			System.out.println("\t" + "7");
-		
+
 		}
-		
+
 		if (a > b) {
-		
+
 			System.out.println("\t" + "8");
-		
+
 		}
-		
+
 		System.out.println();
 	}
 }
--- jd1-les04-ex04/src/by/htp/les04/branching/Task03.java
+++ jd1-les04-ex04/src/by/htp/les04/branching/Task03.java
@@ -4,25 +4,25 @@ public class Task03 {
 // 3. Составить программу сравнения введенного числа а и цифры 3. Вывести на экран слово «yes», если число а меньше 3; если больше, то вывести слово «no».
 	public static void task() {
 		System.out.println("3. Составить программу сравнения введенного числа а и цифры 3. Вывести на экран слово «yes», если число а меньше 3; если больше, то вывести слово «no».");
-				
+
 		int a;
 		a = 4;
-		
-		System.out.println("\t" + "a = " + a );
+
+		System.out.println("\t" + "a = " + a);
 		System.out.println();
-		
+
 		if (a < 3) {
-			
+
 			System.out.println("\t" + "yes");
-			
+
 		}
-		
+
 		if (a > 3) {
-			
+
 			System.out.println("\t" + "no");
-			
+
 		}
-		
+
 		System.out.println();
 	}
 }
